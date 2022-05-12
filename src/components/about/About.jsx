59806cea44b0afa0362e98d9bf9e3671818ba553
Nnamdi <nnamdi.b@gmail.com>
2022-05-12 16:25:49 +0200

    small updates

---
+++ src/components/about/About.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+import './about.css';
+import Morant from '../../img/jamorant.jpg';
+
+export const About = () => {
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img src={Morant} alt="" className="a-img" />
+        </div>
+      </div>
+      <div className="a-right">
+        <h1 className="a-title">About Me</h1>
+        <p className="a-sub">
+          It is a long established fact that a reader will be d readable
+          content.
+        </p>
+        <p className="a-desc">
+          Lorem Ipsum is simply dummy text of the printing and typesetting
+          industry. Lorem Ipsum has been the industry's standard dummy text ever
+          since the 1500s, when an unknown printer took a galley of type and
+          scrambled it to make a type specimen book. It has survived not only
+          five centuries, but also the leap into electronic typesetting,
+          remaining essentially unchanged.
+        </p>
+      </div>
+    </div>
+  );
+};
